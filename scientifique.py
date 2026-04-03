b895e60b9dba5764e80896fe7cb85f7d8ade4a53
Yohane alex Konan <yohanealexkonan@gmail.com>
2026-04-03 15:49:14 +0000

    Update scientifique.py
    Signed-off-by: Yohane alex Konan <yohanealexkonan@gmail.com>

--- scientifique.py
+++ scientifique.py
@@ -1,30 +1,19 @@
 import math
 
-def traiter_expression(expression):
+def sinus(x):
+    return math.sin(x)
 
-    expression = expression.replace('π', str(math.pi))
-    expression = expression.replace('e', str(math.e))
-    expression = expression.replace('^', '**')
-    expression = expression.replace('×', '*')
-    expression = expression.replace('÷', '/')
+def cosinus(x):
+    return math.cos(x)
 
-    expression = expression.replace('sin', 'math.sin')
-    expression = expression.replace('cos', 'math.cos')
-    expression = expression.replace('tan', 'math.tan')
-    expression = expression.replace('sqrt', 'math.sqrt')
-    expression = expression.replace('log', 'math.log10')
-    expression = expression.replace('ln', 'math.log')
+def tangente(x):
+    return math.tan(x)
 
-    return expression
+def logarithme(x):
+    return math.log10(x)
 
+def ln(x):
+    return math.log(x)
 
-def calculer(expression):
-    try:
-        expression = traiter_expression(expression)
-        return eval(expression)
-    except:
-        return "Erreur"
-
-
-def supprimer(texte):
-    return texte[:-1]
+def racine(x):
+    return math.sqrt(x)
